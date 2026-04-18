68809d3dd01d054b3f2867d222864bfae1e9761d
Codega | PHP Software, Hosting and Domain Services — Türkiye <109313760+codegatr@users.noreply.github.com>
2026-04-18 18:05:19 +0300

    Initial: libs/entegrator-gib/CodegaGib/Exception/CodegaGibException.php

---
+++ libs/entegrator-gib/CodegaGib/Exception/CodegaGibException.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CodegaGib\Exception;
+
+/**
+ * CodegaGib kütüphanesindeki tüm istisnalar için temel sınıf.
+ */
+class CodegaGibException extends \Exception
+{
+}
